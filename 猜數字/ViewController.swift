6682ef3d12dc9d37669dc11c0f8f76394a841fe6
ray247k <ray247k@gmail.com>
2017-06-24 10:40:41 +0800

    [clean] 改變變數名稱，增加註解
    times => chance

--- 猜數字/ViewController.swift
+++ 猜數字/ViewController.swift
@@ -9,47 +9,93 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //輸入數字
     @IBOutlet weak var putin: UITextField!
+    
+    //提示
     @IBOutlet weak var hint: UILabel!
+    
+    //可猜的次數
     @IBOutlet weak var time: UILabel!
+    
+    //答對的畫面
     @IBOutlet weak var flower: UIImageView!
+    
+    //答錯的畫面
     @IBOutlet weak var tear: UIImageView!
 
+    //答案初始值
     var answer = 0
-    var times = 6
+    
+    //可猜次數初始值
+    var chance = 6
+
+    //最大範圍提示
     var upbound = 100
+
+    //最小範圍提示
     var lowbound = 0
     
+    /**
+     * 按鈕按下之後檢查猜測次數
+     *
+     * @parma putin.text 輸入的數字
+     * @result hint.text 提示訊息
+     */
     @IBAction func gogo(_ sender: Any) {
+        //收鍵盤
+        putin.resignFirstResponder()
         
-        putin.resignFirstResponder() //收鍵盤
-        if putin.text == "" {
-            hint.text="小狐狸正等著你給牠一個數字" //使用者沒輸入
-        }else if Int(putin.text!) == nil {
-            hint.text="只能輸入數字喔!"  //使用者輸入的不是數字
-        }else if times > 0{
-            let i = Int(putin.text!)!
-            check(test:i)
-        }else{
+        //使用者沒輸入
+        if putin.text == ""
+        {
+            hint.text="小狐狸正等著你給牠一個數字"
+        }
+            
+        //使用者輸入的不是數字
+        else if Int(putin.text!) == nil
+        {
+            hint.text="只能輸入數字喔!"
+        }
+        
+        //猜測次數 > 0 定義inputInt輸入轉成Int，執行 check()
+        else if chance > 0
+        {
+            let inputInt = Int(putin.text!)!
+            check(test : inputInt)
+        }
+        
+        //次數 =< 0
+        else
+        {
             hint.text = "小狐狸哭哭 沒機會了"
             tear.isHidden = false
         }
     }
-        
+    
+    /**
+     * 顯示新的範圍提示訊息
+     * 
+     * @parma void
+     * @return str 新的範圍
+     */
     func getBoundaryText() -> String {
         return "(\(lowbound)~\(upbound))"
     }
     
-    if times >  0 {
-        let i = Int(putin.text!)!
-        check(test: i)
-    }
+    /**
+     * 檢查輸入是否為目標數值
+     *
+     * @parma
+     * @return
+     */
     func check(test:Int)
     {
         if test > 50
         {
             hint.text = "猜的數字超出範圍嚕!\(getBoundaryText())"
-            times -= 1
+            chance -= 1
             time.text = String(chance)
     
         }
@@ -57,7 +103,7 @@ class ViewController: UIViewController {
         {
             upbound = test - 1
             hint.text = "沒有這麼多朵\(getBoundaryText())"
-            times -= 1
+            chance -= 1
             guessTime.text = String(chance)
     
         }
@@ -65,7 +111,7 @@ class ViewController: UIViewController {
         {
             lowbound = test + 1
             hint.text = "太少朵了吧!\(getBoundaryText())"
-            times -= 1
+            chance -= 1
             guessTime.text = String(chance)
         }
         else
@@ -79,7 +125,7 @@ class ViewController: UIViewController {
     
     hint.text = "放膽去猜!!"
     putin.text = "0"
-    times = 6
+    chance = 6
     upbound = 50
     lowbound = 0
     answer = Int(arc4random_uniform(50))
